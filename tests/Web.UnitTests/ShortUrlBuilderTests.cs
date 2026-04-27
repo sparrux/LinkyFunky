c12b65d455c4e807a6ad54f9672d0d14b87e2a7b
Okunev <ilyaokubev@gmail.com>
2026-04-27 09:56:30 +0400

    [AI] Add unit tests for ShortUrlBuilder in Web layer

---
+++ tests/Web.UnitTests/ShortUrlBuilderTests.cs
@@ -0,0 +1,48 @@
+using Microsoft.Extensions.Options;
+using Web.Options;
+using Web.Services;
+
+namespace Web.UnitTests;
+
+public class ShortUrlBuilderTests
+{
+    [Fact]
+    public void Constructor_ShouldThrowArgumentException_WhenBaseUrlIsInvalid()
+    {
+        var options = CreateDomainOptions("not-a-valid-url");
+
+        var exception = Assert.Throws<ArgumentException>(() => new ShortUrlBuilder(options));
+
+        Assert.Equal("domainOptions", exception.ParamName);
+        Assert.Equal("Domain:BaseUrl must be a valid absolute URL. (Parameter 'domainOptions')", exception.Message);
+    }
+
+    [Theory]
+    [InlineData("https://linkyfunky.com", "abc123", "https://linkyfunky.com/abc123")]
+    [InlineData("https://linkyfunky.com/", "abc123", "https://linkyfunky.com/abc123")]
+    [InlineData("https://linkyfunky.com", "/abc123", "https://linkyfunky.com/abc123")]
+    [InlineData("https://linkyfunky.com/", "/abc123", "https://linkyfunky.com/abc123")]
+    [InlineData("https://linkyfunky.com", "///abc123", "https://linkyfunky.com/abc123")]
+    public void Build_ShouldReturnExpectedShortUrl(string baseUrl, string shortCode, string expectedShortUrl)
+    {
+        var sut = CreateSut(baseUrl);
+
+        var shortUrl = sut.Build(shortCode);
+
+        Assert.Equal(expectedShortUrl, shortUrl);
+    }
+
+    static ShortUrlBuilder CreateSut(string baseUrl)
+    {
+        var domainOptions = CreateDomainOptions(baseUrl);
+        return new(domainOptions);
+    }
+
+    static IOptions<DomainOptions> CreateDomainOptions(string baseUrl)
+    {
+        return Microsoft.Extensions.Options.Options.Create(new DomainOptions
+        {
+            BaseUrl = baseUrl
+        });
+    }
+}
